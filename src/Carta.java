0b5d463fc22420b3685b128c1716ffa59a71a844
juangrela96 <grela_96@hotmail.com>
2020-10-07 23:12:28 -0300

    toma gato

--- src/Carta.java
+++ src/Carta.java
@@ -1,11 +1,69 @@
 
 public class Carta {
 	
-	private float altura;
+	private String nombre;
+	private int altura;
+	private int peso;
+	private int fuerza;
+	private int peleas_gandas;
+	private int velocidad;
 	
-	
-	public Carta() {
-		
+	public Carta(String nombre, int altura, int peso, int fuerza, int peleas_gandas, int velocidad) {
+		this.nombre = nombre;
+		this.altura = altura;
+		this.peso = peso;
+		this.fuerza = fuerza;
+		this.peleas_gandas = peleas_gandas;
+		this.velocidad = velocidad;
 	}
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getAltura() {
+		return altura;
+	}
+
+	public void setAltura(int altura) {
+		this.altura = altura;
+	}
+
+	public int getPeso() {
+		return peso;
+	}
+
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+
+	public int getFuerza() {
+		return fuerza;
+	}
+
+	public void setFuerza(int fuerza) {
+		this.fuerza = fuerza;
+	}
+
+	public int getPeleas_gandas() {
+		return peleas_gandas;
+	}
+
+	public void setPeleas_gandas(int peleas_gandas) {
+		this.peleas_gandas = peleas_gandas;
+	}
+
+	public int getVelocidad() {
+		return velocidad;
+	}
+
+	public void setVelocidad(int velocidad) {
+		this.velocidad = velocidad;
+	}
+	
+	
 }
--- src/Mazo.java
+++ src/Mazo.java
@@ -1,8 +1,23 @@
+import java.util.ArrayList;
 
 public class Mazo {
 
-	public Mazo() {
-		// TODO Auto-generated constructor stub
+	private ArrayList<Carta> cartas;
+	
+	public Mazo(ArrayList<Carta> cartas) {
+		this.cartas = cartas;
 	}
 
+	public void removeCarta() {
+		cartas.remove(0);
+	}
+	
+	public void addCarta(Carta c) {
+		cartas.add(c);
+	}
+	
+	public void abarajar() {
+		ArrayList<Carta> aux = new ArrayList<Carta>();
+		
+	}
 }
